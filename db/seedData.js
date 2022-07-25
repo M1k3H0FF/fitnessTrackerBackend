41c19bd990487f7eff9af8a6259d48fdf7999d55
michaelHoffman <mphoffman8390@gmail.com>
2022-07-25 14:26:02 -0500

    on to activities

--- db/seedData.js
+++ db/seedData.js
@@ -1,15 +1,39 @@
 // require in the database adapter functions as you write them (createUser, createActivity...)
-// const { } = require('./');
+//const {createUser} = require('./');
 const client = require("./client")
+const {createUser} = require('./users');
+
 
 async function dropTables() {
+  try{
   console.log("Dropping All Tables...")
-  // drop all tables, in the correct order
+  await client.query(`
+  DROP TABLE IF EXISTS mytablename
+  `)
+  console.log("Finsihed droping tables!")
+  } catch (error) {
+    console.log("Error dropping tables")
+    throw error;
+  }
 }
 
 async function createTables() {
+  try{
   console.log("Starting to build tables...")
-  // create all tables, in the correct order
+  
+  await client.query(`
+  CREATE TABLE mytablename(
+    id SERIAL PRIMARY KEY,
+        username varchar(255) UNIQUE NOT NULL,
+        password varchar(255) NOT NULL
+  );
+  `)
+
+  console.log("Finished building tables!");
+    } catch (error) {
+      console.error("Error building tables!");
+      throw error; 
+  }
 }
 
 /* 
