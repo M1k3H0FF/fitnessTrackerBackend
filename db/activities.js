fdf9d8152f4a4ad091e61379ee4c4d8011f02180
Hunter Norris <hunter.t.norris@gmail.com>
2022-07-27 12:45:03 -0500

    done with activities

--- db/activities.js
+++ db/activities.js
@@ -21,19 +21,66 @@ catch (error) {
 
 async function getAllActivities() {
   const { rows } = await client.query(`
-    SELECT name 
+    SELECT * 
     FROM activities;
   `)
   return rows;
 }
 
-async function getActivityById(id) {}
+async function getActivityById(id) {
+  try {
+    const { rows: [ activity ] } = await client.query(`
+      SELECT *
+      FROM activities
+      WHERE id=$1;
+    `, [id]);
 
-async function getActivityByName(name) {}
+    return activity;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getActivityByName(name) {
+  try {
+    const { rows: [ activity ] } = await client.query(`
+      SELECT *
+      FROM activities
+      WHERE name=$1;
+    `, [name]);
+
+    return activity;
+  } catch (error) {
+    throw error;
+  }
+}
 
 async function attachActivitiesToRoutines(routines) {}
 
 async function updateActivity({ id, ...fields }) {
+  // build the set string
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${ key }"=$${ index + 1 }`
+  ).join(', ');
+      console.log(setString, 'Here is the setString')
+  // // return early if this is called without fields
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const { rows: [activity]} = await client.query(`
+      UPDATE activities
+      SET ${ setString }
+      WHERE id=${ id }
+      RETURNING *;
+    `, Object.values(fields));
+
+    return activity;
+  } catch (error) {
+    throw error;
+  }
+  
   // don't try to update the id
   // do update the name and description
   // return the updated activity
