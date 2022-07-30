2d757d6e2b090190188d2b817a4c48d588efe640
Hunter Norris <hunter.t.norris@gmail.com>
2022-07-30 16:19:14 -0500

    1.5 activities GETS

--- api/activities.js
+++ api/activities.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const {getAllActivities, getPublicRoutinesByActivity } = require('../db')
 
 // GET /api/activities/:activityId/routines
+router.get('/:activityId/routines', async (req, res, next)=> {
+    const { activityId } = req.params;
+    const obj = {id: activityId}
+    try {
+        if (!activityId){
+            return error
+        }
+        const pablo = await getPublicRoutinesByActivity(obj);
+        console.log(pablo, "dudette")
+        res.send(pablo)
+
+
+    } catch (error) {
+        next(error)
+    }
+})
 
 // GET /api/activities
+router.get('/', async (req, res, next)=>{
+    try{
+        const allActivities = await getAllActivities()
+
+        res.send(allActivities)
+
+    } catch(error) {
+        next(error)
+    }
+});
 
 // POST /api/activities
 
