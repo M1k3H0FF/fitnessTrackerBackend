d0c194ff8404da5b3b1d7207c3cce49fae80632a
Hunter Norris <hunter.t.norris@gmail.com>
2022-07-30 21:00:38 -0500

    Activity Tests: 3 fails, 5 passes

--- api/activities.js
+++ api/activities.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const {getAllActivities, getPublicRoutinesByActivity } = require('../db')
+const {getAllActivities, getPublicRoutinesByActivity, createActivity, getActivityById } = require('../db');
+const { ActivityNotFoundError } = require('../errors');
 
 // GET /api/activities/:activityId/routines
 router.get('/:activityId/routines', async (req, res, next)=> {
     const { activityId } = req.params;
     const obj = {id: activityId}
-    try {
-        if (!activityId){
-            return error
-        }
-        const pablo = await getPublicRoutinesByActivity(obj);
-        console.log(pablo, "dudette")
-        res.send(pablo)
+    const activity = await getActivityById(activityId)
 
+    try {
+        if (!activity){
+            next({
+                error: 'ActivityNotFoundError',
+                message: `Activity ${ activityId } not found`,
+                name: 'ActivityNotFoundError'
+            });
+        }
+        const publicRoutinesByActivity = await getPublicRoutinesByActivity(obj);
+
+        res.send(publicRoutinesByActivity)
 
     } catch (error) {
         next(error)
@@ -33,7 +39,39 @@ router.get('/', async (req, res, next)=>{
 });
 
 // POST /api/activities
+router.post('/', async (req, res, next) =>{
+    
+    if(!req.user) {
+        next({
+            name: 'MissingUserError',
+            message: 'You must be logged in to perform this action'
+        });
+    }
+
+    const { name, description } = req.body;
+    const activityData = {name: name, description:description}
+
+    const newActivity = await createActivity(activityData)
+    res.send(newActivity)
+});
 
 // PATCH /api/activities/:activityId
+router.patch('/:activityId', async (req, res, next)=>{
+    const { activityId } = req.params;
+    const obj = {id: activityId}
+
+    const activity = await getActivityById(activityId)
+    try {
+        if (!activity){
+            res.send({
+                error: 'ActivityNotFoundError',
+                message: `Activity ${ activityId } not found`,
+                name: 'ActivityNotFoundError'
+            });
+        }
+    } catch (error) {
+        next(error)
+    }
+});
 
 module.exports = router;
